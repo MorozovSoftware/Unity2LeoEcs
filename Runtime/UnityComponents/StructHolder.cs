2f6f96ea7460d8a5063ad7bc51e1e9a7d81e2177
MorozovSoftware <77966009+MorozovSoftware@users.noreply.github.com>
2022-09-07 19:59:44 +0500

    Struct in inspector

---
+++ Runtime/UnityComponents/StructHolder.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MorozovSoftware.Unity2LeoEcs
+{
+    public abstract class StructHolder<T> : MonoBehaviour where T : struct
+    {
+        [SerializeField] protected T value;
+    }
+}
+
