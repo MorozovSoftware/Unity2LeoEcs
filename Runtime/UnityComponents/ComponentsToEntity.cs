8491bb440f13fb3455923b5c1e3a461158ee30e4
MorozovSoftware <77966009+MorozovSoftware@users.noreply.github.com>
2022-09-15 03:56:17 +0500

    Added ComponentsToEntity.cs

---
+++ Runtime/UnityComponents/ComponentsToEntity.cs
@@ -0,0 +1,56 @@
+using Leopotam.Ecs;
+using MorozovSoftware.Unity2LeoEcs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace MorozovSoftware.Unity2LeoEcs
+{
+    public abstract class ComponentsToEntity : MonoBehaviour
+    {
+        List<Component> _components;
+        EcsEntity _entity;
+
+        [Inject]
+        public void Construct(List<Component> components, EcsEntity entity)
+        {
+            _components = components;
+            _entity = entity;
+        }
+
+        protected void Replace()
+        {
+            foreach (var item in _components)
+            {
+                if (item is IStructForLeoEcs structForLeoEcs)
+                {
+                    structForLeoEcs.Replace(_entity);
+                }
+                else
+                {
+                    _entity.AddUnityObjectByReflection(item);
+                }
+            }
+        }
+
+        protected void Del()
+        {
+            if (_entity.IsWorldAlive())
+            {
+                foreach (var item in _components)
+                {
+                    if (item is IStructForLeoEcs structForLeoEcs)
+                    {
+                        structForLeoEcs.Del(_entity);
+                    }
+                    else
+                    {
+                        _entity.DelUnityObjectByReflection(item);
+                    }
+                }
+            }
+        }
+    }
+}
+    
