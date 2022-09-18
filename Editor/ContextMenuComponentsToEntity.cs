1fe5e224586dff3fce1a5d8d86be9ec82c1f3b1d
MorozovSoftware <77966009+MorozovSoftware@users.noreply.github.com>
2022-09-19 00:53:54 +0500

    Update ContextMenuComponentsToEntity.cs

--- Editor/ContextMenuComponentsToEntity.cs
+++ Editor/ContextMenuComponentsToEntity.cs
@@ -1,58 +1,48 @@
 using UnityEditor;
-using UnityEngine;
 
 namespace MorozovSoftware.Unity2LeoEcs.Editor
 {
     public class ContextMenuComponentsToEntity
     {
-        [MenuItem("GameObject/Unity2LeoEcs/Active", true, 25)]
-        static bool ValidateActive()
+        [MenuItem("GameObject/Unity 2 LeoECS/Select Active To Entity", true, 25)]
+        static bool ValidateSelectActive()
         {
             return Selection.activeTransform != null;
         }
 
-        [MenuItem("GameObject/Unity2LeoEcs/Alive", true, 25)]
-        static bool ValidateAlive()
+        [MenuItem("GameObject/Unity 2 LeoECS/Select Alive To Entity", true, 25)]
+        static bool ValidateSelectAlive()
         {
             return Selection.activeTransform != null;
         }
 
-        [MenuItem("GameObject/Unity2LeoEcs/Started", true, 25)]
-        static bool ValidateStarted()
+        [MenuItem("GameObject/Unity 2 LeoECS/Select Started To Entity", true, 25)]
+        static bool ValidateSelectStarted()
         {
             return Selection.activeTransform != null;
         }
 
         
         
-        [MenuItem("GameObject/Unity2LeoEcs/Active", false, 25)]
-        static void InstallActive()
+        [MenuItem("GameObject/Unity 2 LeoECS/Select Active To Entity", false, 25)]
+        static void SelectActive()
         {
-            DestroyComponentsToEntity();
+            Selection.activeTransform.gameObject.DestroyImmediate<ComponentsToEntity>();
             Selection.activeTransform.gameObject.AddComponent<ActiveToEntity>();
         }
 
-        [MenuItem("GameObject/Unity2LeoEcs/Alive", false, 25)]
-        static void InstallAlive()
+        [MenuItem("GameObject/Unity 2 LeoECS/Select Alive To Entity", false, 25)]
+        static void SelectAlive()
         {
-            DestroyComponentsToEntity();
+            Selection.activeTransform.gameObject.DestroyImmediate<ComponentsToEntity>();
             Selection.activeTransform.gameObject.AddComponent<AliveToEntity>();
         }
 
-        [MenuItem("GameObject/Unity2LeoEcs/Started", false, 25)]
-        static void InstallStarted()
+        [MenuItem("GameObject/Unity 2 LeoECS/Select Started To Entity", false, 25)]
+        static void SelectStarted()
         {
-            DestroyComponentsToEntity();
+            Selection.activeTransform.gameObject.DestroyImmediate<ComponentsToEntity>();
             Selection.activeTransform.gameObject.AddComponent<StartedToEntity>();
-        }
-        
-        
-        static void DestroyComponentsToEntity()
-        {
-            foreach (Component item in Selection.activeTransform.gameObject.GetComponents<ComponentsToEntity>())
-            {
-                Object.DestroyImmediate(item);
-            }
         }
     }
 }
